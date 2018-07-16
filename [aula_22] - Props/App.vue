554a1bebc65c2bb6fd67ba894a97a6093213ee2c
João Paulo Farias <joaof@perseus.local>
2018-07-16 13:31:51 -0300

    cmt_6

---
+++ [aula_22] - Props/App.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		<app-header></app-header>
+		<app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
+		<app-footer></app-footer>
+	</div>
+</template>
+
+<script>
+
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import Ninjas from './components/Ninjas.vue';
---
+++ projeto-vue/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<h1>{{ title }}</h1>
+		<p>{{ saudacao }}</p>
+	</div>
+</template>
+
+<script>
+export default{
+	data(){
+		return{
+			title: 'Teste Vue 1...'
+		}
+	},
+	methods:{
+		saudacao: function(){
+			return 'E aee!';
+		}
+	}
+}
+</script>
