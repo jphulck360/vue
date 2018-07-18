46e8dae1d17b9fd6537b3bc285a5b590b4c045f3
João Paulo Farias <joaof@perseus.local>
2018-07-18 13:21:32 -0300

    7.1

---
+++ [aula_29] - Input Binding (Create Blog)/addBlog.vue
@@ -0,0 +1,32 @@
+<template>
+	<div id="add-blog">
+		<h2>Adicionar novo post</h2>
+		<form>
+			<label>Título</label>
+			<input type="text" v-model.lazy="blog.title" required />
+			<label>Conteúdo</label>
+			<textarea v-model.lazy="blog.content"></textarea>
+		</form>
+		<div id="preview">
+			<h3>Visualizar Blog</h3>
+			<p>Título: {{ title }}</p>
+			<p>Conteúdo: {{ content }}</p>
+		</div>
+	</div>
+</template>
+
+<script>
+
+export default{
+	data(){
+		return{
+			blog:{
+				title: "",
+				content: ""
+			}
+		}
+	},
+	methods:{
+	}
+}
+</script>
