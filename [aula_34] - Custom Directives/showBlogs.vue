50a7497001afa646407e01a23102a63178f3d281
João Paulo Farias <joaof@perseus.local>
2018-07-25 13:23:00 -0300

    atz_1.2

---
+++ [aula_34] - Custom Directives/showBlogs.vue
@@ -0,0 +1,38 @@
+<template>
+	<div id="show-blogs">
+		<h1>Lista de Artigos</h1>
+		<div v-for="blog in blogs" class="single-blog">
+			<h2>{{ blog.title }}</h2>
+			<article>{{ blog.body }}</article>
+		</div>
+	</div>
+</template>
+
+<script>
+
+export default{
+	data(){
+		return{
+			blogs: []
+		}
+	},
+	methods:{
+	},
+	created(){
+		this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(data)){
+			this.blogs = data.body.slice(0,10);
+			
+		}
+	}
+}
+</script>
+
+<style>
+
+#show-blogs{
+}
+
+.single-blog{
+	padding: 20px;
+}
+</style>
