08f87a5e5b4033a53abc5b965419a55c1cfb45c3
João Paulo Farias <joaof@perseus.local>
2018-07-30 13:19:04 -0300

    atz_1.4

---
+++ [aula_37] - Registering Things Locally/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource);
+
+// Filtros
+
+Vue.filter('snippet', function(value){
+	return value.slice(0,100) + '...';
+});
+
+new Vue({
+	el: '#app',
+	render: h => h(App)
+})
